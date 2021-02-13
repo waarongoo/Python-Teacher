0fb27381ec0422ee654bc11cd563e31ebfdb729b
aww7 <wuaaron12345@gmail.com>
2021-02-13 02:22:00 +0000

    Added more code to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from termcolor import colored
 import sys
 import time
 
+
 def lesson3():
   print("Time to practice that you just learned")
   user = input("How do you print something in the console ")
@@ -13,11 +14,11 @@ def lesson3():
     print("The answer is print()")
 
 def lesson2():
-  print("Ok so for this next lesson we are gonna use something called turtle. What is turtle? It is like the animal turtle you may ask? No, its a python package for drawing. Is it slow? No its very fast depending on REPLs computer resources\n\n\n")
-  print("In this package, we can do many different things with the turtle. Now choose what do you want to do with the turtle.\n\n\n")
+  print("Ok so for this next lesson we are gonna use something called turtle. What is turtle? It is like the animal turtle you may ask? No, its a python package for drawing. Is it slow? No its very fast depending on REPLs computer resources haha.\n\n\n")
+  print("In this package, we can do many different things with the 'turtle'. Now choose what do you want to do with the turtle.\n\n")
 
 
-  print("Welcome to the Python Turtle IDE\n\n\n\n")
+  print("Welcome to the Python Turtle IDE\n\n")
   go = True
   while go == True:
     choice = input("Enter 1 to move the turtle forward, 2 to move backwards, 3 to turn left, 4 to turn right, 5 to clear your drawing. 6, to stop drawing\n")
@@ -51,7 +52,7 @@ def startlesson1():
   if user == 'print("Hello World")':
     print("Hello World")
     print("Nice job! Alright lets go to the next lesson")
-    lesson3()
+    lesson2()
   else:
     print("Please enter the exact values above")
     startlesson1()
@@ -61,6 +62,8 @@ for x in colored("Welcome To the Online Python Teacher\n", "white"):
 	sys.stdout.flush()
 	time.sleep(0.05)
 name = input("Hello my name is Robert your virtual teacher. What is your name?\n>>>")
+
+
 doing = input("Hello " + name + " how are you doing?\n>>>")
 print("This project was inspired by COVID-19 Distance Learning Copyright awdev.")
 for x in colored("\nAll rights reserved.\n", "red"):
